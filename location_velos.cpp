92fee6d15cc1447e290a96039902c68775102cd5
Abdou AFIFI (kali) <afifiabdou@gmail.com>
2020-03-30 08:07:54 +0000

    etape 1

--- location_velos.cpp
+++ location_velos.cpp
@@ -3,79 +3,33 @@ using namespace std;
 
 int main()
 {
-  cout << "Donnez l'heure de début de la location (un entier) : ";
+  cout << "Donnez l'heure de dÃ©but de la location (un entier) : ";
   int debut;
   cin >> debut;
 
   cout << "Donnez l'heure de fin de la location (un entier) : ";
-
-
   int fin;
   cin >> fin;
 
+  /*****************************************************
+   * ComplÃ©ter le code Ã  partir d'ici
+   *****************************************************/
 
-  /****************************************************
-   * Compléter le code à partir d'ici
-   ****************************************************/
+    cout << "Les heures doivent Ãªtre comprises entre 0 et 24 !" << endl;
+    cout << "Bizarre, vous n'avez pas louÃ© votre vÃ©lo bien longtemps !" << endl;
+    cout << "Bizarre, le dÃ©but de la location est aprÃ¨s la fin ..." << endl;
 
+    cout << "Vous avez louÃ© votre vÃ©lo pendant" << endl;
 
-  if (debut < 0 or debut > 24 or fin <0 or fin > 24 )
-  {
-    cout << "Les heures doivent être comprises entre 0 et 24 !" << endl;
-  }
-  else if (debut == fin)
-  {
-      cout << "Bizarre, vous n'avez pas loué votre vélo bien longtemps !" << endl;
-  }
+      cout << " heure(s) au tarif horaire de "
+           << " franc(s)" << endl;
 
-  else if (debut > fin)
-  {
-      cout << "Bizarre, le début de la location est après la fin ..." << endl;
-
-  }
-
-  else
-  {
-      int unf(0);
-      int deuxf(0);
-      if(debut<= 7 and fin>= 17)
-      {
-          unf = (7-debut) + (fin - 17);
-          deuxf = 10;
-      }
-
-      if(debut<= 7 and fin< 17)
-      {
-          unf = (7-debut) ;
-          deuxf =  (fin - 17);
-      }
-
-      if(debut> 7 and fin>= 17)
-      {
-          unf = (fin - 17);
-          deuxf = (17 - debut) ;
-      }
-
-      if(debut> 7 and fin>= 17)
-      {
-          unf = 0;
-          deuxf = (fin - debut) ;
-      }
-
-
-    cout << "Vous avez loué votre vélo pendant" << endl;
-
-    cout <<unf << " heure(s) au tarif horaire de "
-          <<1 << " franc(s)" << endl;
-
-    cout <<deuxf<< " heure(s) au tarif horaire de "
-      <<2 << " franc(s)" << endl;
-
-    cout << "Le montant total à payer est de " << unf + (deuxf)* 2
+    cout << "Le montant total Ã  payer est de "
          << " franc(s)." << endl;
-}
+
   /*******************************************
-   * Ne rien modifier après cette ligne.
+   * Ne rien modifier aprÃ¨s cette ligne.
    *******************************************/
+
   return 0;
 }
