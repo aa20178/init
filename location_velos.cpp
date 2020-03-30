76a2bc128897924ba1c09ac2eecc90c22fb38dbc
Abdou AFIFI (kali) <afifiabdou@gmail.com>
2020-03-30 08:25:09 +0000

    etape 2

--- location_velos.cpp
+++ location_velos.cpp
@@ -15,18 +15,62 @@ int main()
    * Compléter le code à partir d'ici
    *****************************************************/
 
+
+  if (debut < 0 or debut > 24 or fin <0 or fin > 24 )
+  {
     cout << "Les heures doivent être comprises entre 0 et 24 !" << endl;
-    cout << "Bizarre, vous n'avez pas loué votre vélo bien longtemps !" << endl;
-    cout << "Bizarre, le début de la location est après la fin ..." << endl;
+  }
+  else if (debut == fin)
+  {
+      cout << "Bizarre, vous n'avez pas loué votre vélo bien longtemps !" << endl;
+  }
+
+  else if (debut > fin)
+  {
+      cout << "Bizarre, le début de la location est après la fin ..." << endl;
+  }
+
+  else
+  {
+      int unf(0);
+      int deuxf(0);
+      if(debut<= 7 and fin>= 17)
+      {
+          unf = (7-debut) + (fin - 17);
+          deuxf = 10;
+      }
+
+      if(debut<= 7 and fin< 17)
+      {
+          unf = (7-debut) ;
+          deuxf =  (fin-7);
+      }
+
+      if(debut> 7 and fin>= 17)
+      {
+          unf = (fin - 17);
+          deuxf = (17 - debut) ;
+      }
+
+      if(debut> 7 and fin<17)
+      {
+          unf = 0;
+          deuxf = (fin - debut) ;
+      }
+
 
     cout << "Vous avez loué votre vélo pendant" << endl;
 
-      cout << " heure(s) au tarif horaire de "
-           << " franc(s)" << endl;
+    cout <<unf << " heure(s) au tarif horaire de "
+          <<1 << " franc(s)" << endl;
 
-    cout << "Le montant total à payer est de "
+    cout <<deuxf<< " heure(s) au tarif horaire de "
+      <<2 << " franc(s)" << endl;
+
+    cout << "Le montant total à payer est de " << unf + (deuxf)* 2
          << " franc(s)." << endl;
 
+  }
   /*******************************************
    * Ne rien modifier après cette ligne.
    *******************************************/
