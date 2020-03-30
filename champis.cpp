1deb4abf871da7deb9f0abd73019922730ea2de1
Abdou Afifi <afifiabdou@gmail.com>
2020-03-30 13:02:46 +0200

    champis

---
+++ champis.cpp
@@ -0,0 +1,16 @@
+  cout << "Pensez Ã  un champignon : amanite tue-mouches, pied bleu, girolle," << endl
+       << "cÃ¨pe de Bordeaux, coprin chevelu ou agaric jaunissant." << endl << endl;
+  
+  cout << "Est-ce que votre champignon a des lamelles (1 : oui, 0 : non) ? ";
+  cout << "Est-ce que votre champignon a un anneau (1 : oui, 0 : non) ? ";
+  cout << "Est-ce que votre champignon a un chapeau convexe (1 : oui, 0 : non) ? ";
+  cout << "Est-ce que votre champignon vit en forÃªt (1 : oui, 0 : non) ? ";
+
+  cout << "==> Le champignon auquel vous pensez est ";
+
+  cout << "l'agaric jaunissant";
+  cout << "l'amanite tue-mouches";
+  cout << "le cÃ¨pe de Bordeaux";
+  cout << "le coprin chevelu";
+  cout << "la girolle";
+  cout << "le pied bleu";
