afb375056c26bf6aff0e3a6efddb60a024d47d0b
Abdou Afifi <afifiabdou@gmail.com>
2020-04-01 12:52:16 +0200

    lire_et_dire

---
+++ semaine4/lireetdire.cc
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+int separer_chiffre_gauche(int& nombre)
+{
+  int dix(1);
+  int temp(nombre);
+  while (temp >= 10) {
+    dix  *= 10;
+    temp /= 10;
+  }
+  nombre %= dix;
+  return temp;
+}
+
+/*****************************************************
+ * Compléter le code à partir d'ici
+ *****************************************************/
+
+void ajouter_chiffre_droit(int& nombre, int chiffre)
+{
+}
+
+void dire_chiffre(int& nombre, int repetitions_chiffre, int chiffre)
+{
+}
+
+int lire_et_dire(int nombre)
+{
+}
+
+/*******************************************
+ * Ne rien modifier après cette ligne.
+ *******************************************/
+
+void repeter_lire_et_dire(int& nombre, int fois)
+{
+  while (fois-- > 0) {
+    nombre = lire_et_dire(nombre);
+  }
+}
+
+int main()
+{
+  int nombre(1);
+  int fois(1);
+  cin >> nombre >> fois;
+  repeter_lire_et_dire(nombre, fois);
+  cout << nombre << endl;
+  return 0;
+}
