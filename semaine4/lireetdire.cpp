7ae4df0896b96489fb923baecc2fc727cb9f946e
Abdou AFIFI (kali) <afifiabdou@gmail.com>
2020-04-02 14:14:44 +0000

    avancer lire et dire

--- semaine4/lireetdire.cpp
+++ semaine4/lireetdire.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 using namespace std;
 
-int separer_chiffre_gauche(int& nombre)
+int separer_chiffre_gauche(int &nombre)
 {
   int dix(1);
   int temp(nombre);
-  while (temp >= 10) {
-    dix  *= 10;
+  while (temp >= 10)
+  {
+    dix *= 10;
     temp /= 10;
   }
   nombre %= dix;
@@ -17,25 +18,60 @@ int separer_chiffre_gauche(int& nombre)
  * Compléter le code à partir d'ici
  *****************************************************/
 
-void ajouter_chiffre_droit(int& nombre, int chiffre)
+void ajouter_chiffre_droit(int &nombre, int chiffre)
 {
+  nombre = (nombre * 10) + chiffre;
 }
 
-void dire_chiffre(int& nombre, int repetitions_chiffre, int chiffre)
+void dire_chiffre(int &nombre, int repetitions_chiffre, int chiffre)
 {
+
+  ajouter_chiffre_droit(nombre, repetitions_chiffre);
+  ajouter_chiffre_droit(nombre, chiffre);
 }
 
 int lire_et_dire(int nombre)
 {
+  int chiffre_gauche(0), chiffre_gauche_while(0), repetitions(1), resultat(0);
+  chiffre_gauche = separer_chiffre_gauche(nombre);
+  while (nombre != 0)
+  {
+    chiffre_gauche_while = separer_chiffre_gauche(nombre);
+
+    if (chiffre_gauche == chiffre_gauche_while)
+    {
+      ++repetitions;
+      
+    }
+    else
+    {
+      dire_chiffre(resultat, repetitions, chiffre_gauche);
+      repetitions=1;
+      chiffre_gauche = chiffre_gauche_while;
+    }
+  }
+  if (nombre == 0)
+  {
+    dire_chiffre(resultat, repetitions, chiffre_gauche);
+  }
+
+  nombre = resultat ;
+  resultat = 0 ;
+  chiffre_gauche = 0 ; 
+  chiffre_gauche_while = 0;
+  repetitions = 1 ; 
+  
+  return nombre ; 
 }
 
 /*******************************************
  * Ne rien modifier après cette ligne.
  *******************************************/
 
-void repeter_lire_et_dire(int& nombre, int fois)
+void repeter_lire_et_dire(int &nombre, int fois)
 {
-  while (fois-- > 0) {
+  while (fois-- > 0)
+  {
     nombre = lire_et_dire(nombre);
   }
 }
@@ -45,7 +81,7 @@ int main()
   int nombre(1);
   int fois(1);
   cin >> nombre >> fois;
-  repeter_lire_et_dire(nombre, fois);
+  nombre = lire_et_dire(nombre);//, fois);
   cout << nombre << endl;
   return 0;
 }
