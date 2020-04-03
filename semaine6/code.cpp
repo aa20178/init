7e7cb6e32decb50a3ae9842ce21bc16dd526ec74
Abdou AFIFI (kali) <afifiabdou@gmail.com>
2020-04-03 10:17:33 +0000

    semaine 6 part char

---
+++ semaine6/code.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+char code(char c, int d)
+{
+    if ( (c > 'a') and  (c <= 'z'))
+    {
+        return decale(c,'a', d );
+    }
+
+    else if ((c > 'A') and  (c <= 'Z'))
+    {
+        return decale(c,'A', d );
+
+    }
+
+    else
+    {
+        return c ; 
+    }
+    
+}
+
+int decale(char c, char debut, int decalage)
+{
+    while (decalage < 0)
+    {
+        decalage += 26;
+    }
+    return debut + (((c - debut) + decalage) % 26) ; 
+}
+
+string code(string c, int d)
+{ }
+
+
+int main()
+{
+    reutrn 0 ; 
+}
