98d82989d352895553da7283015ca89bd2aa430d
Abdou AFIFI (kali) <afifiabdou@gmail.com>
2020-04-07 15:06:40 +0000

    labo

---
+++ oop/labo.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Souris
+{
+  /*****************************************************
+    Compléter le code à partir d'ici
+  *******************************************************/
+
+  /*******************************************
+   * Ne rien modifier après cette ligne.
+   *******************************************/
+
+}; // fin de la classe Souris
+
+int main()
+{
+  Souris s1(50.0, "blanche", 2);
+  Souris s2(45.0, "grise");
+  Souris s3(s2);
+  // ... un tableau peut-être...
+  s1.afficher();
+  s2.afficher();
+  s3.afficher();
+  s1.evolue();
+  s2.evolue();
+  s3.evolue();
+  s1.afficher();
+  s2.afficher();
+  s3.afficher();
+  return 0;
+}
