add3a4cad2a05021f58febf231290597a3db3826
Abdou AFIFI (kali) <afifiabdou@gmail.com>
2020-04-12 22:07:19 +0000

    ref

--- oop/semaine3/_6257cb4e4f85889c9857e2a947c9f95e_chimie.cc
+++ oop/semaine3/_6257cb4e4f85889c9857e2a947c9f95e_chimie.cc
@@ -15,8 +15,54 @@ public:
     Compléter le code à partir d'ici
   *******************************************************/
 
+  double getVolume() const
+  {
+    return volume;
+  }
+
+  double getpH() const
+  {
+    return pH;
+  }
+
+  string getNom() const
+  {
+    return nom;
+  }
+
+  Flacon(string n, double v, double p) : nom(n), volume(v), pH(p){};
+
+  ostream &etiquette(ostream &sortie) const
+  {
+    sortie << getNom() << " : " << getVolume() << " ml, pH " << getpH();
+    return sortie;
+  }
+
+  Flacon operator+(const Flacon &f1)
+  {
+    double pow1 = pow(10.0, -f1.getpH());
+    double pow2 = pow(10.0, getpH());
+
+    double num2 = (getVolume() * pow2);
+    double num1 = (f1.getVolume() * pow1);
+    double num = num1 + num2;
+    double den = f1.getVolume() + getVolume();
+    double newph = -log10(num / den);
+    Flacon ff(f1.getNom() + " + " + getNom(), f1.getVolume() + getVolume(), newph);
+    return ff;
+  }
 };
 
+Flacon operator+(Flacon f1, const Flacon &f2)
+{
+  f1.operator+(f2);
+  return f1;
+}
+
+ostream &operator<<(ostream &sortie, const Flacon &f)
+{
+  return f.etiquette(sortie);
+}
 /*******************************************
  * Ne rien modifier après cette ligne.
  *******************************************/
