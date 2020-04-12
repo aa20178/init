4aa931e59fcc397d0cb5de3e3704a9659f4714c2
Abdou Afifi <afifiabdou@gmail.com>
2020-04-12 11:03:08 +0200

    Add files via upload

---
+++ oop/construction.cc
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+// Pour simplifier
+typedef string Forme   ;
+typedef string Couleur ;
+
+class Brique
+{
+private:
+  Forme   forme   ;
+  Couleur couleur ;
+
+public:
+  /*****************************************************
+    Compléter le code à partir d'ici
+  *******************************************************/
+
+};
+
+class Construction
+{
+  friend class Grader;
+
+};
+
+const Construction operator*(unsigned int n, Construction const& a)
+{
+}
+
+const Construction operator/(unsigned int n, Construction const& a)
+{
+}
+
+const Construction operator%(unsigned int n, Construction const& a)
+{
+}
+
+/*******************************************
+ * Ne rien modifier après cette ligne.
+ *******************************************/
+
+int main()
+{
+  // Modèles de briques
+  Brique toitD("obliqueD", "rouge");
+  Brique toitG("obliqueG", "rouge");
+  Brique toitM(" pleine ", "rouge");
+  Brique mur  (" pleine ", "blanc");
+  Brique vide ("                 ", "");
+
+  unsigned int largeur(4);
+  unsigned int profondeur(3);
+  unsigned int hauteur(3); // sans le toit
+
+  // on construit les murs
+  Construction maison( hauteur / ( profondeur % (largeur * mur) ) );
+
+  // on construit le toit
+  Construction toit(profondeur % ( toitG + 2*toitM + toitD ));
+  toit ^= profondeur % (vide + toitG + toitD);
+
+  // on pose le toit sur les murs
+  maison ^= toit;
+
+  // on admire notre construction
+  cout << maison << endl;
+
+  return 0;
+}
