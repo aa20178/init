a0b4887709a7f8a4e100fa80080d0c287011335f
Abdou Afifi <afifiabdou@gmail.com>
2020-09-12 21:11:32 +0200

    Update construction.cpp

--- oop/semaine3/construction.cpp
+++ oop/semaine3/construction.cpp
@@ -17,7 +17,6 @@ public:
   /*****************************************************
     Compléter le code à partir d'ici
   *******************************************************/
-
 Brique(Forme f, Couleur c) : forme(f),couleur(c) {} 
 Brique()=delete ;
 
@@ -35,48 +34,136 @@ ostream& afficher(ostream& sortie) const
 }
 
  
-
 };
 
  ostream& operator<<(ostream& sortie, Brique const& b)  
  {
-	 return b.afficher(sortie) ;
-	 
+	 return b.afficher(sortie) ;	 
+ }
+ 
+
+
+class Construction
+{
+  friend class Grader;
+
+public:
+vector<vector<vector<Brique>>> contenu ;
+Construction(Brique b) : contenu(1, vector<vector<Brique>>(1, vector<Brique>(1,b))){}
+  Construction & operator ^= (Construction const &b);
+    Construction&  operator-=(Construction const& b)  ;
+    Construction&  operator+=(Construction const& b)  ;
+
+ostream& afficher(ostream& sortie) const 
+{
+	if (contenu.empty())
+	{
+		return sortie ; 
+	}
+	else 
+	{
+        for (size_t i = contenu.size(); i--; ) 
+        {
+			sortie << "couche " << i << " :\n"  ; 				
+			for (size_t k = 0 ; k< contenu[i].size() ; k++)
+			{
+				for (size_t j = 0 ; j< contenu[i][k].size() ; j++)
+				{
+					sortie << contenu[i][k][j] ;		
+				}
+				sortie << "\n" ; 
+			}
+			
+    	}
+	    return sortie ;
+	}
+}
+
+  
+  
+};
+
+
+  Construction & Construction::operator^= (Construction const &b)
+ {
+     for(size_t i(0); i< b.contenu.size();i++)
+	  {
+	      contenu.push_back(b.contenu[i]);
+	  }
+	return (*this);
+	  
+ }
+  Construction&  Construction::operator-=(Construction const& b)  
+ {
+          if(b.contenu.size() >= contenu.size())
+          {
+              for(size_t i(0); i< contenu.size();i++)
+              {
+                for(size_t j(0); j< b.contenu[i].size();j++)
+                {
+                    contenu[i].push_back(b.contenu[i][j]);
+                    
+                }
+              }
+          }
+          	return (*this);
+
  }
  
  
- class Construction
-{
-friend class Grader;
-
-public:
-vector<vector<vector<int>>> contenu ;
-Construction(Brique b) : contenu(1, vector<vector<int>>(1, vector<int>(1,5))){}
-ostream& afficher(ostream& sortie) const 
-{
-	int i(0) ;
-	for (int i =  contenu.size() ; i>0 ; i--)
+Construction&  Construction::operator+= (Construction const& b)
+  {
+    int flag = 1;
+      
+    if (b.contenu.size () < contenu.size ())
 	{
-		sortie << "couche " << i << " :"  ; 				
-	    
-		for (int k = 0 ; k< contenu[i].size() ; k++)
-		{
-			for (int j = 0 ; j< contenu[i][k].size() ; j++)
-			{
-				sortie << contenu[i][k][j] ;		
-			}
-			sortie << "\n" ; 
-		
-		}
-		
-	}
-	return sortie ;
+	    flag = 0;
+    }
+    else
+    {
+        
+	    for (size_t i (0); i < contenu.size (); i++)
+    	{
+        	if (b.contenu[i].size () < contenu[i].size ())
+        	{
+        	    flag = 0; 
+        	    break ; 
+        	}
+        	
+    	}
+    	if(flag == 1)
+    	{
+    	    
+    	    for (size_t i (0); i < contenu.size (); i++)
+        	{
+                	for (size_t j (0);j <contenu[i].size (); j++)
+                	{
+    
+                    	for (size_t k (0); k < b.contenu[i][j].size (); k++)
+                    	{
+        	                contenu[i][j].push_back (b.contenu[i][j][k]);
+                    	}
+        	
+    	            
+                	}
+        	}
+        }
+    }
+    
+    return (*this);
+
 }
 
 
+ 
+ 
 
-};
+ ostream& operator<<(ostream& sortie, Construction const& b)  
+ {
+	 return b.afficher(sortie) ;	 
+ }
 
+ 
 /*******************************************
  * Ne rien modifier après cette ligne.
  *******************************************/
@@ -85,36 +172,22 @@ int main()
 {
   // Modèles de briques
   Brique toitD("obliqueD", "rouge");
-  Brique toitG("obliqueG", "rouge");
+  Brique toitG("obliqueG", "blanc");
   Brique toitM(" pleine ", "rouge");
   Brique mur  (" pleine ", "blanc");
   Brique vide ("                 ", "");
-  
-    cout << mur << endl << toitD;
-
-  
-
-  unsigned int largeur(4);
-  unsigned int profondeur(3);
-  unsigned int hauteur(3); // sans le toit
-  
-  Construction con(mur); 
-  cout << con.afficher(cout);
 
   // on construit les murs
- 
-/*
- Construction maison( hauteur / ( profondeur % (largeur * mur) ) );
+  Construction maisonblanche(toitG );
+  Construction maisonrouge(toitD );
 
-  // on construit le toit
-  Construction toit(profondeur % ( toitG + 2*toitM + toitD ));
-  toit ^= profondeur % (vide + toitG + toitD);
+  maisonblanche += maisonrouge ; 
+    maisonblanche -= maisonrouge ; 
 
-  // on pose le toit sur les murs
-  maison ^= toit;
+  //maisonblanche ^= maisonrouge ; 
 
-  // on admire notre construction
-  cout << maison << endl;
-*/
+  
+  cout << maisonblanche << endl;
+
   return 0;
 }
